906a0a4bec54e7f82245816403eec178ad8c48ef
xydzy1 <xydzy@mail.ustc.edu.cn>
2023-08-07 18:36:46 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
-	<div>Vue 2</div>
+	<div class="absolute h-620 w-1440 left-0 top-0 opcity-100 
+	bg-gradient-tl from bg-p1color1 from-0% via-p1color2 via-55% to-p1color3 to-100% ">
+		<div class="fixed bg-transparent top-0" id="nav">
+
+		</div>	
+	</div>
 </template>
+
+<style>
+
+</style>
+
+<script>
+	window.onscroll=function(){
+		var topScroll =document.body.scrollTop;
+		var nav=document.getElementById("nav");
+		if(topScroll>120){
+			nav.style.backgroundColor = 'rgba(0,0,0,0.75)'
+		}
+	}
+	
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // main.js
 import Vue from "vue";
 import App from "./App.vue"
+import './input.css'
 
 new Vue({
    el: "#app",
